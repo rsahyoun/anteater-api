4b17276c601e5e1d28312762730f0f4cb43b78c9
Sanskar Mishra <69398106+sanskarm7@users.noreply.github.com>
2025-06-05 19:19:25 -0500

    Revert "Library Traffic Endpoint" (#179)
    Reverts icssc/anteater-api#178

--- apps/data-pipeline/library-traffic-scraper/src/handler.ts
+++
@@ -1,9 +0,0 @@
-import { database } from "@packages/db";
-import { doScrape } from "./lib";
-
-export default {
-  async scheduled(_, env) {
-    const db = database(env.DB.connectionString);
-    await doScrape(db);
-  },
-} satisfies ExportedHandler<Env>;
