rsahyoun/anteater-api

commit 03f11f7e3873c2a49227013a06eae7b439833bd4

feat(programs): 4-year CHC requirements (#183)
## Description

Allow the collection of the 4-year CHC program block, if found.
As mentioned on the issue, we must be scraping from an account in the
four-year CHC program.
As of yet we do not actually know whether the requirements block we
obtain is for four-year CHC versus two-year CHC or another variant.
We need to see both blocks side-by-side to learn how to tell them apart.

~~**Blocked by #181.**~~

## Related Issue

#182 

## Types of changes

- [ ] Bug fix (non-breaking change which fixes an issue)
- [x] New feature (non-breaking change which adds functionality)
- [ ] Breaking change (fix or feature that would cause existing
functionality to change)

## Checklist:

- [x] My code involves a change to the database schema.
- [ ] My code requires a change to the documentation.

apps/data-pipeline/degreeworks-scraper/src/index.ts
@@ -31,6 +31,7 @@ async function main() {
   } = scraper.get();
   const ucRequirementData = parsedUgradRequirements.get("UC");
   const geRequirementData = parsedUgradRequirements.get("GE");
+  const honorsFourRequirementData = parsedUgradRequirements.get("CHC4");
 
   const degreeData = degreesAwarded
     .entries()
@@ -102,6 +103,21 @@ async function main() {
           {
             id: "GE",
             requirements: geRequirementData,
+          },
+        ])
+        .onConflictDoUpdate({
+          target: schoolRequirement.id,
+          set: conflictUpdateSetAllCols(schoolRequirement),
+        });
+    }
+
+    if (honorsFourRequirementData) {
+      await tx
+        .insert(schoolRequirement)
+        .values([
+          {
+            id: "CHC4",
+            requirements: honorsFourRequirementData,
           },
         ])
         .onConflictDoUpdate({

apps/api/src/schema/programs.ts
@@ -44,7 +44,7 @@ export const specializationRequirementsQuerySchema = z.object({
   }),
 });
 
-export const UgradRequirementsBlockIds = ["UC", "GE"] as const;
+export const UgradRequirementsBlockIds = ["UC", "GE", "CHC4"] as const;
 
 export const ugradRequirementsQuerySchema = z.object({
   id: z.enum(UgradRequirementsBlockIds).openapi({ description: "The requirements block to fetch" }),

apps/data-pipeline/degreeworks-scraper/src/components/DegreeworksClient.ts
@@ -1,4 +1,4 @@
-import type { Block, DWAuditResponse, DWMappingResponse } from "$types";
+import type { Block, DWAuditResponse, DWMappingResponse, UndergraduateRequirements } from "$types";
 import fetch from "cross-fetch";
 
 export class DegreeworksClient {
@@ -44,7 +44,7 @@ export class DegreeworksClient {
       .join("&");
   }
 
-  async getUgradRequirements(): Promise<[Block, Block] | undefined> {
+  async getUgradRequirements(): Promise<UndergraduateRequirements | undefined> {
     const params = DegreeworksClient.formatQueryParams({
       studentId: this.studentId,
       // more schools are possible, see this.getMapping("schools"), but we want undergrad requirements
@@ -73,7 +73,15 @@ export class DegreeworksClient {
       return;
     }
 
-    return [ucRequirements, geRequirements];
+    const honorsFourRequirements = json.blockArray.find(
+      (b) => b.requirementType === "OTHER" && b.requirementValue === "CHP",
+    );
+
+    return {
+      UC: ucRequirements,
+      GE: geRequirements,
+      CHC4: honorsFourRequirements,
+    };
   }
 
   /**

apps/data-pipeline/degreeworks-scraper/src/components/Scraper.ts
@@ -229,7 +229,7 @@ export class Scraper {
       return;
     }
 
-    const [ucRequirements, geRequirements] = ugradReqs;
+    const { UC: ucRequirements, GE: geRequirements, CHC4: honorsFourRequirements } = ugradReqs;
     this.parsedUgradRequirements.set(
       "UC",
       await this.ap.ruleArrayToRequirements(ucRequirements.ruleArray),
@@ -238,7 +238,17 @@ export class Scraper {
       "GE",
       await this.ap.ruleArrayToRequirements(geRequirements.ruleArray),
     );
-    console.log("Fetched university and GE requirements");
+    if (honorsFourRequirements) {
+      this.parsedUgradRequirements.set(
+        "CHC4",
+        await this.ap.ruleArrayToRequirements(honorsFourRequirements.ruleArray),
+      );
+    } else {
+      console.warn(
+        "no access to honors requirements; retry scrape from honors-enrolled account to get this information",
+      );
+    }
+    console.log("Fetched university, GE, and 4-year honors requirements");
 
     this.degrees = await this.dw.getMapping("degrees");
     console.log(`Fetched ${this.degrees.size} degrees`);

apps/data-pipeline/degreeworks-scraper/src/types.ts
@@ -77,6 +77,17 @@ export type Block = {
   title: string;
   ruleArray: Rule[];
 };
+
+export interface UndergraduateRequirements {
+  // university of california-wide requirements, expressed in UCI coursework
+  UC: Block;
+  // general education requirements
+  GE: Block;
+  // requirements for the four year campus honors collegium program
+  CHC4: Block | undefined;
+  // TODO: two-year/transfer CHC here
+}
+
 export type DWAuditOKResponse = { blockArray: Block[] };
 export type DWAuditErrorResponse = { error: never };
 /**

packages/db/migrations/0017_widen_school_requirement_id.sql
@@ -1,0 +1,1 @@
+ALTER TABLE "school_requirement" ALTER COLUMN "id" SET DATA TYPE varchar;

packages/db/src/schema.ts
@@ -2,7 +2,6 @@ import type { SQL } from "drizzle-orm";
 import { and, eq, getTableColumns, isNotNull, ne, sql } from "drizzle-orm";
 import {
   boolean,
-  char,
   date,
   decimal,
   index,
@@ -620,7 +619,7 @@ export const degree = pgTable("degree", {
 });
 
 export const schoolRequirement = pgTable("school_requirement", {
-  id: char("id", { length: 2 }).primaryKey(),
+  id: varchar("id").primaryKey(),
   requirements: json("requirements").$type<DegreeWorksRequirement[]>().notNull(),
 });
 

commit 1aa1e1c0d32fdc9cb55b5911ce82a6717efc0b37

fix(degreeworks-scraper): incorrect construction of new database entity (#227)
Fixes issue introduced in #183.
This is a simple typo.
This only affects the first stage of ETL, so existing SQL rows,
including those in production, continue to be accessible as if this
issue did not exist.

apps/data-pipeline/degreeworks-scraper/src/index.ts
@@ -144,7 +144,7 @@ async function main() {
 
     for (const majorObj of majorData) {
       if (majorObj.collegeBlockIndex !== undefined) {
-        (majorObj as typeof major.$inferInsert).college =
+        (majorObj as typeof major.$inferInsert).collegeRequirement =
           collegeBlockIds[majorObj.collegeBlockIndex];
       }
     }